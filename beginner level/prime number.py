309f9cb4f414e1fecad7eb4e20b2f672df078be2
Adharshclinton <36730566+Adharshclinton@users.noreply.github.com>
2018-07-24 10:51:06 +0530

    Update prime number.py

--- beginner level/prime number.py
+++ beginner level/prime number.py
@@ -1,9 +1,9 @@
-num=int(input())
-if num>1:
- for i in range(2, num):
-   if (num%i == 0):
-	   print(" no it is not a prime number")
-	   break
+n=input()
+if n>1:
+ for i in range(2, n):
+   if ((n%i)==0):
+	   print("no it is not a prime number")
+	   break:
    else:
 	   print("yes it is a prime number")
 else:
